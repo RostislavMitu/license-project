21af515d6c168136c1fddd70eacffbc1360ba065
Mitu Rostislav <rmitu@tacitknowledge.com>
2018-03-18 17:12:50 +0200

    Set up webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, 'src/index'),
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+}
