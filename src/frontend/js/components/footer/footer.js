efe2af4e7768d06a188012f219826b2780ee543a
ASUS-N580V\Cristi <cristi.duluta@gmail.com>
2018-05-16 21:32:11 +0300

    Add files weren't include in git

---
+++ src/frontend/js/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render() {
+    return (
+      <div>
+        <footer className="small-1">
+          <div className='footer'>
+            <a className="about">About</a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/frontend/js/components/homePage/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ApartmentsListPage from '../../apartments/apartmentsListPage';
+
+const HomePage = () => (
+      <div className="homePage">
+        <ApartmentsListPage/>
+      </div>
+);
+
+export default HomePage;
